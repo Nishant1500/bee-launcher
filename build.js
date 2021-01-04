b1dca2a13eee08709f3944aa744bad2b5f93ebe0
Nishant1500 <53296643+Nishant1500@users.noreply.github.com>
2021-01-04 18:49:11 +0530

    Added Files For New Release

---
+++ build.js
@@ -0,0 +1,69 @@
+const builder = require('electron-builder')
+const Platform = builder.Platform
+
+function getCurrentPlatform(){
+    switch(process.platform){
+        case 'win32':
+            return Platform.WINDOWS
+        case 'darwin':
+            return Platform.MAC
+        case 'linux':
+            return Platform.linux
+        default:
+            console.error('Cannot resolve current platform!')
+            return undefined
+    }
+}
+
+builder.build({
+    targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
+    config: {
+        appId: 'beelauncher',
+        productName: 'Bee Launcher',
+        artifactName: '${productName}-setup-${version}.${ext}',
+        copyright: 'Copyright © 2021 Nishant1500',
+        directories: {
+            buildResources: 'build',
+            output: 'builds'
+        },
+        win: {
+            icon: './assets/icon.ico',
+            target: [
+                {
+                    target: 'nsis',
+                    arch: 'x64'
+                }
+            ]
+        },
+        nsis: {
+            oneClick: false,
+            perMachine: false,
+            allowElevation: true,
+            allowToChangeInstallationDirectory: true
+        },
+        mac: {
+            target: 'dmg',
+            category: 'public.app-category.games'
+        },
+        linux: {
+            target: 'AppImage',
+            maintainer: 'Nishant1500',
+            vendor: 'Nishant1500',
+            synopsis: 'Modded Minecraft Launcher',
+            description: 'Custom launcher which allows users to join modded servers. All mods, configurations, and updates are handled automatically.',
+            category: 'Game'
+        },
+        compression: 'maximum',
+        files: [
+            '!{dist,.gitignore,.vscode,docs,dev-app-update.yml,.travis.yml,.nvmrc,.eslintrc.json,build.js}'
+        ],
+        extraResources: [
+            'libraries',
+        ],
+        asar: true
+    }
+}).then(() => {
+    console.log('Build complete!')
+}).catch(err => {
+    console.error('Error during build!', err)
+})
